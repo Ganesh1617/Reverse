7e78b1a0faf1787adf4500196f7b1adccbc74501
Ganesh1617 <151633666+Ganesh1617@users.noreply.github.com>
2023-12-14 09:48:14 +0530

    Initial commit

---
+++ Reverse/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reverse
+{
+    internal class Program
+    {
+        public void reverse()
+        {
+            int n = 123, r = 0;
+            while (n > 0)
+            {
+                r = (n % 10);
+                n /= 10;
+                Console.Write(r);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            p.reverse();
+            Console.ReadLine();
+        }
+    }
+}
